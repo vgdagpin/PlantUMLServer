bcaf9f7336135d06ede44b2d40b02832ddb36b42
Vincent Dagpin <vincent.dagpin@towerswatson.com>
2023-10-29 05:29:58 +0800

    Added endpoints

--- PlantUMLServer/Controllers/HomeController.cs
+++ PlantUMLServer/Controllers/HomeController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PlantUml.Net;
-using System.IO;
 using System.Net;
-using System.Reflection;
 using System.Text;
+using System.Text.Json;
 
 namespace PlantUMLServer.Controllers
 {
@@ -11,29 +10,95 @@ namespace PlantUMLServer.Controllers
     [Route("[controller]")]
     public class HomeController : ControllerBase
     {
+        private readonly IPlantUmlRenderer plantUmlRenderer;
+
+        public HomeController(IPlantUmlRenderer plantUmlRenderer)
+        {
+            this.plantUmlRenderer = plantUmlRenderer;
+        }
+
         [HttpGet]
         [Route("/")]
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var vsixPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var path = Path.Combine(vsixPath, "PlantUML", "plantuml.jar");
-
-            var r = new RendererFactory()
-                .CreateRenderer(new PlantUmlSettings
+            return new ContentResult()
+            {
+                Content = JsonSerializer.Serialize(new[]
                 {
-                    JavaPath = path
-                });
+                    new
+                    {
+                        Type = "Get",
+                        Url = "/Render/{base64data}",
+                        Body = (string?)null
+                    },
+                    new
+                    {
+                        Type = "Post",
+                        Url = "/RenderFromBase64",
+                        Body = (string?)"base64 data"
+                    },
+                    new
+                    {
+                        Type = "Post",
+                        Url = "/RenderFromPlain",
+                        Body = (string?)"uml data"
+                    }
+                }),
+                ContentType = "application/json",
+                StatusCode = (int)HttpStatusCode.OK
+            };
+        }
+        
+        [HttpGet]
+        [Route("/Render/{base64data}")]
+        public async Task<IActionResult> Render(string base64data)
+        {
+            var bData = Convert.FromBase64String(base64data);
+            var strData = Encoding.UTF8.GetString(bData);
 
-            var d = @"@startuml
-Bob -> Alice : hello
-@enduml";
-
-            var temp = await r.RenderAsync(d, OutputFormat.Svg);
+            var result = await plantUmlRenderer.RenderAsync(strData, OutputFormat.Svg);
 
             return new ContentResult()
             {
-                Content = Encoding.UTF8.GetString(temp),
+                Content = Encoding.UTF8.GetString(result),
                 ContentType = "image/svg+xml",
+                StatusCode = (int)HttpStatusCode.OK
+            };
+        }
+
+        [HttpPost]
+        [Route("/RenderFromBase64")]
+        public async Task<IActionResult> RenderFromBase64()
+        {
+            var stream = new StreamReader(Request.Body);
+            var reqBody = await stream.ReadToEndAsync();
+
+            var bData = Convert.FromBase64String(reqBody);
+            var strData = Encoding.UTF8.GetString(bData);
+
+            var result = await plantUmlRenderer.RenderAsync(strData, OutputFormat.Svg);
+
+            return new ContentResult()
+            {
+                Content = Encoding.UTF8.GetString(result),
+                ContentType = "text/xml",
+                StatusCode = (int)HttpStatusCode.OK
+            };
+        }
+
+        [HttpPost]
+        [Route("/RenderFromPlain")]
+        public async Task<IActionResult> RenderFromPlain()
+        {
+            var stream = new StreamReader(Request.Body);
+            var reqBody = await stream.ReadToEndAsync();
+
+            var result = await plantUmlRenderer.RenderAsync(reqBody, OutputFormat.Svg);
+
+            return new ContentResult()
+            {
+                Content = Encoding.UTF8.GetString(result),
+                ContentType = "text/xml",
                 StatusCode = (int)HttpStatusCode.OK
             };
         }
--- PlantUMLServer/Program.cs
+++ PlantUMLServer/Program.cs
@@ -1,8 +1,28 @@
+using PlantUml.Net;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<IPlantUmlRenderer>(sp =>
+{
+    var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    if (string.IsNullOrWhiteSpace(assemblyPath))
+    {
+        throw new ApplicationException("Assembly path not found");
+    }
+
+    var path = Path.Combine(assemblyPath, "PlantUML", "plantuml.jar");
+
+    return new RendererFactory()
+               .CreateRenderer(new PlantUmlSettings
+               {
+                   JavaPath = path
+               });
+});
 
 var app = builder.Build();
 
